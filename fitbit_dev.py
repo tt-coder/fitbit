9901dc859a697c553099c105a7d75ab1c9f37592
tt-coder <tokio.tm@gmail.com>
2017-09-05 18:52:19 +0900

    success get sleep data

--- fitbit_dev.py
+++ fitbit_dev.py
@@ -2,11 +2,12 @@ import fitbit
 import json
 import auth_data as ad
 
-DATE = "2017-08-06"
+DATE = "2017-09-04"
 
-authd_client = fitbit.Fitbit(ad.CLIENT_ID, ad.CLIENT_SECRET, access_token=ad.ACCESS_TOKEN, refresh_token=ad.REFRESH_TOKEN)
-data = authd_client.intraday_time_series('activities/steps', base_date="2017-09-04", detail_level='1min', start_time="07:00", end_time="09:00")
-print(json.dumps(data, indent=4))
+authd_client = fitbit.Fitbit(ad.CLIENT_ID, ad.CLIENT_SECRET,access_token=ad.ACCESS_TOKEN, refresh_token=ad.REFRESH_TOKEN)
+#data = authd_client.intraday_time_series('activities/heart', base_date="2017-09-04", detail_level='1min', start_time="07:00", end_time="09:00")
+data = authd_client.sleep(date=DATE)
+print(json.dumps(data["sleep"][0]["minuteData"][0], indent=4))
 
 
 #authd_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
